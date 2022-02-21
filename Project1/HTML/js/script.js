908e0ea730f27d20d0406f2341581ee859e5144e
vikram mahara <vmahara1999@gmail.com>
2022-02-21 11:28:38 +0530

    first commit

---
+++ Project1/HTML/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 50) {
+            $('#Header').addClass('shrink');
+        }
+        else {
+            $('#Header').removeClass('shrink');
+        }
+
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+
+    $('#scroll').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+
+
+});
+
